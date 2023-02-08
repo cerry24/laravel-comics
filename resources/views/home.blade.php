2da5e21051914be9065a25c04da19fdc48572489
cerry24 <samuelecerretti@gmail.com>
2023-02-08 16:32:09 +0100

    implement jumbotron & comics db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,7 @@
 @extends('layouts.app')
+
+@section('main-content')
+    <section id="jumbotron">
+        <img src="{{ Vite::asset('resources/assets/img/jumbotron.jpg') }}" alt="">
+    </section>
+@endsection
