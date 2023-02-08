54073e135ee9559d47dae49b4d266cfe489f13f1
cerry24 <samuelecerretti@gmail.com>
2023-02-08 16:15:12 +0100

    implement header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,8 @@
     </head>
 
     <body>
+        @include('partials.header')
+
         <main>
             @yield('main-content')
         </main>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,50 @@
+<header class="bg-white">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg">
+            <a class="navbar-brand" href="#">
+                <img src="{{ Vite::asset('resources/assets/img/dc-logo.png') }}" alt="Dc logo" width="50" height="50">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">characters</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-primary" href="#">comics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">movies</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">tv</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">games</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">collectibles</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">videos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">fans</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">news</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">shop</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2 user-input" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-primary" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    </div>
+</header>
